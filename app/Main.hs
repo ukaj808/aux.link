467567ab77f36064b7b1b0693cc741c14f4b3a03
fisnik <fisnik.ukaj@justeattakeaway.com>
2023-01-18 15:20:30 +0100

    Parsing command line options

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,10 @@ module Main ( main ) where
 import Network.Wai.Handler.Warp (run)
 
 import Augslink.Service.Application (server)
+import CommandLine (getOptions)
 
 main :: IO ()
-main = run 8080 server
+main = do
+  options <- getOptions
+  print options
+  run 8080 server
