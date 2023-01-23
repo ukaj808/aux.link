7fad2e4bdd53dcd12f84b747e07666269fa09923
fisnik <ukaj808@gmail.com>
2023-01-23 23:19:59 +0100

    Added custom HTML type for explictness; difference between Raw for static directory and returning a single HTML Page

--- app/AugsLink/Service/API.hs
+++ app/AugsLink/Service/API.hs
@@ -1,7 +1,28 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module AugsLink.Service.API
   ( API
+  , HTML(..)
+  , RawHtml(..)
   ) where
 
+import Data.ByteString.Lazy as Lazy
+import Network.HTTP.Media ((//), (/:))
 import Servant
 
-type API = "public" :> Raw
+data HTML =
+  HTML
+
+newtype RawHtml =
+  RawHtml
+    { unRaw :: Lazy.ByteString
+    }
+
+instance Accept HTML where
+  contentType _ = "text" // "html" /: ("charset", "utf-8")
+
+instance MimeRender HTML RawHtml where
+  mimeRender _ = unRaw
+
+type API
+   = Get '[ HTML] RawHtml :<|> Get '[ HTML] RawHtml :<|> "room" :> Capture "roomid" String :> Get '[ HTML] RawHtml :<|> "public" :> Raw
--- app/AugsLink/Service/Application.hs
+++ app/AugsLink/Service/Application.hs
@@ -1,16 +1,22 @@
 module AugsLink.Service.Application ( server ) where
 
-import Data.Proxy (Proxy(..))
-import Network.Wai (Application)
-import Servant (Server, serveDirectoryWebApp)
-import Servant.Server (serve)
+import Data.ByteString.Lazy as Lazy
+import Servant 
 
 import AugsLink.Service.API (API)
 import CommandLine (Options (staticFilePath))
 
-
 handlers :: Options -> Server API
-handlers opts = serveDirectoryWebApp (staticFilePath opts)
+handlers opts = home
+  :<|> create
+  :<|> room 
+  :<|> public
+
+  where 
+    create = undefined
+    room id = undefined
+    home = Lazy.readFile "/public/pages/home/home.html"
+    public = serveDirectoryWebApp (staticFilePath opts)
 
 server :: Options -> Application
 server opts = serve (Proxy @API) (handlers opts)
