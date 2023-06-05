4a283559b4421b6e31d3767b7a087c45de6585d0
Fisnik <ukaj808@gmail.com>
2023-06-05 10:32:11 +0200

    sortablejs with file drop api??

--- app/AugsLink/Service/Handlers/GetRoom.hs
+++ app/AugsLink/Service/Handlers/GetRoom.hs
@@ -67,10 +67,10 @@ renderCurrentlyPlayingSection =
 renderDropSection :: H.Html
 renderDropSection = 
   H.section ! A.id "drop"      ! A.class_ "full-flex centered default-margin secondary-theme"  $ do
-    H.label ! A.for "drop-zone" ! A.class_ "full-flex centered column draggable" $ do
+    H.label ! A.id "drop-zone" ! A.for "drop-zone-input" ! A.class_ "full-flex centered column draggable" $ do
       musicIconSvg
       H.h4 "Upload an audio file or paste a link to get your queue ready in time for your turn!"
-      H.input ! A.type_ "file" ! A.id "drop-zone" ! A.accept "audio/*" ! A.style "display:none"
+      H.input ! A.type_ "file" ! A.id "drop-zone-input" ! A.accept "audio/*" ! A.style "display:none"
 
 renderRoomPage :: [RoomUser] -> H.Html
 renderRoomPage users = H.docTypeHtml $ do
