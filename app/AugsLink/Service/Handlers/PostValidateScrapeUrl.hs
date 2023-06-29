07df4bf989e10bd86d43228754957657defee8ac
fisnik <ukaj808@gmail.com>
2023-06-29 21:32:03 +0200

    Shaping the scrape url upload apis

--- app/AugsLink/Service/Handlers/PostValidateScrapeUrl.hs
+++ app/AugsLink/Service/Handlers/PostValidateScrapeUrl.hs
@@ -3,12 +3,17 @@ module AugsLink.Service.Handlers.PostValidateScrapeUrl
   validateHandler
 ) where
 
+import Control.Monad.Cont
 import Data.Text
 import Servant
 
 import AugsLink.Core.API
 import AugsLink.Service.API
-import Control.Monad.Cont
+import Commons.YoutubeDl
 
 validateHandler :: ScrapeUrlValidateRequest -> Handler Text
-validateHandler = undefined
+validateHandler req = do
+  valid <- liftIO $ isValidUrl (u req)
+  if valid
+    then return $ pack ""
+    else throwError err400
---
+++ app/Commons/YoutubeDl.hs
@@ -0,0 +1,13 @@
+module Commons.YoutubeDl
+  (
+    isValidUrl,
+    download
+  ) where
+
+import Data.Text
+
+isValidUrl :: Text -> IO Bool
+isValidUrl = undefined
+
+download :: Text -> FilePath -> IO ()
+download = undefined
