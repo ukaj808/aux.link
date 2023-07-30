db4c9cf29e1b48b28695056e9f11dc8751743002
fisnik <ukaj808@gmail.com>
2023-07-30 21:02:46 +0200

    migrating to xstate v5

--- static/modules/currently-playing/currently-playing-element.ts
+++ static/modules/currently-playing/currently-playing-element.ts
@@ -18,11 +18,172 @@ import { fromPollingToDisconnected } from "./transitions/from-polling/from-polli
 import { fromPollingToStreaming } from "./transitions/from-polling/from-polling-to-streaming";
 import { fromStreamingToCountdown } from "./transitions/from-streaming/from-streaming-to-countdown";
 import { fromStreamingToDisconnected } from "./transitions/from-streaming/from-streaming-to-disconnected";
+import { createMachine, fromPromise, interpret } from "xstate";
 
 export type CurrentlyPlayingState = 'Connecting' | 'Disconnected' | MusicStreamerState;
 
 export class CurrentlyPlayingElement {
 
+  private machine = createMachine({
+    id: 'currently-playing',
+    initial: 'Disconnected',
+    states: {
+      Disconnected: {
+        exit: 'removeOverlay',
+        on: {
+          CONNECT: { target: 'Connecting' },
+        },
+      },
+      Connecting: {
+        entry: ['showLoading', 'showDisconnectButton'],
+        exit: 'hideLoading',
+        on: {
+          DISCONNECT: { target: 'Disconnected' },
+        },
+        invoke: {
+          src: 'connect',
+          onDone: [
+            {
+              guard: 'isNotRunning',
+              target: 'NotRunning',
+            },
+            {
+              guard: 'isStreaming',
+              target: 'Streaming',
+            },
+            {
+              guard: 'isCountingDown',
+              target: 'Countdown',
+            },
+            {
+              guard: 'isPolling',
+              target: 'Polling',
+            },
+          ],
+          onError: {
+            target: 'Disconnected',
+            //todo: action
+          },
+        },
+      },
+      NotRunning: {
+        entry: ['showWaitingForCreator', 'subscribeToSongStartingEvent'],
+        exit: 'hideWaitingForCreator',
+        on: {
+          COUNTDOWN_STARTED:{ target: 'Countdown' }, 
+          DISCONNECT: { target: 'Disconnected' },
+        },
+      },
+      Countdown: {
+        entry: 'showCountdown',
+        exit: ['unsubscribeFromSongStartingEvent', 'hideCountdown'],
+        on: {
+          COUNTDOWN_FINISHED: 'Polling',
+          DISCONNECT: { target: 'Disconnected' },
+        },
+      },
+      Polling: {
+        entry: ['subscribeToSongUploadedEvent', 'showLoading'],
+        exit: ['unsubscribeFromSongUploadedEvent', 'hideLoading'],
+        on: {
+          SONG_UPLOADED: { target: 'Streaming' },
+          TIMEOUT: { target: 'Countdown' },
+          DISCONNECT: { target: 'Disconnected' },
+        },
+      },
+      Streaming: {
+        entry: ['subscribeToSongStartingEvent', 'showSongTitle', 'startVisualizer'],
+        exit: ['hideSongTitle', 'stopVisualizer'],
+        on: {
+          SONG_FINISHED: 'Countdown', 
+          DISCONNECT: 'Disconnected',
+        },
+      },
+    },
+  }, 
+  {
+    actors: {
+      connect: fromPromise(() => {
+        this.auxAudioPlayer.startListening();
+        return this.restClient.getRoom();
+      }),
+    },
+    actions: {
+      removeOverlay: () => {
+        this.overlayEl.removeEventListener("click", this.overlayClickHandler);
+        this.overlayEl.classList.add('invisible');
+      },
+      showLoading: () => {
+        this.loadingBars.classList.remove('hidden');
+      },
+      hideLoading: () => {
+        this.loadingBars.classList.add('hidden');
+      }, 
+      showCountdown: () => {
+        this.countdownTimer.classList.remove('hidden');
+      },
+      hideCountdown: () => {
+        this.countdownTimer.classList.add('hidden');
+      },
+      showDisconnectButton: () => {
+        this.disconnectBtn.classList.remove('hidden');
+      },
+      hideDisconnectButton: () => {
+        this.disconnectBtn.classList.add('hidden');
+      },
+      showSongTitle: ({context}) => {
+        console.log('showStreaming', context);
+        this.description.classList.remove('hidden');
+        this.description.innerText = "Streaming...";
+      },
+      hideSongTitle: () => {
+        this.description.classList.add('hidden');
+        this.description.innerText = "";
+      },
+      startVisualizer: () => {
+        this.audioVisualizer.start();
+      },
+      stopVisualizer: () => {
+        this.audioVisualizer.stop();
+      },
+      showWaitingForCreator: () => {
+        this.description.classList.remove("hidden");
+        this.description.innerText = "Waiting for the creator to start the music...";
+      },
+      hideWaitingForCreator: () => {
+        this.description.classList.add("hidden");
+        this.description.innerText = "";
+      },
+      subscribeToSongUploadedEvent: () => {
+        this.roomMessageListener.subscribe('SongUploadedEvent', this.songUploadedEventHandler);
+      },
+      unsubscribeFromSongUploadedEvent: () => {
+        this.roomMessageListener.unsubscribe('SongUploadedEvent', this.songUploadedEventHandler);
+      },
+      subscribeToSongStartingEvent: () => {
+        this.roomMessageListener.subscribe('SongStartingEvent', this.songStartingEventHandler);
+      },
+      unsubscribeFromSongStartingEvent: () => {
+        this.roomMessageListener.unsubscribe('SongStartingEvent', this.songStartingEventHandler);
+      },
+    },
+    guards: {
+      isNotRunning: ({event}) => {
+        return event.output.currentlyPlayingView.musicState === 'NotRunning';
+      },
+      isStreaming: ({event}) => {
+        return event.output.currentlyPlayingView.musicState === 'Streaming';
+      },
+      isCountingDown: ({event}) => {
+        return event.output.currentlyPlayingView.musicState === 'Countdown';
+      },
+      isPolling: ({event}) => {
+        return event.output.currentlyPlayingView.musicState === 'Polling';
+      },
+    },
+  });
+
+  private actor = interpret(this.machine);
   private el: HTMLElement;
   private xState: CurrentlyPlayingState;
   private roomMessageListener: RoomMessageListener;
@@ -40,22 +201,30 @@ export class CurrentlyPlayingElement {
   private loadingBars: HTMLDivElement;
   private transitionHistory: CurrentlyPlayingState[];
 
-  private overlayClickHandler: () => void = () => this.transitionTo('Connecting');
+  private overlayClickHandler = () => 
+  {
+    console.log(this.actor);
+    this.actor.send({ type: 'CONNECT' })
+  };
 
   private songStartingEventHandler: (roomEvent: RoomMessage) => void = (data) => {
       const songStartingEvent = data as SongStartingEvent;  
+      this.countdownTimer.innerText = songStartingEvent.s.toString();
       if (songStartingEvent.s === 5) {
-        this.transitionTo('Countdown', this.roomMessageListener);
-      } 
+        this.actor.send({ type: 'COUNTDOWN_STARTED' });
+      } else if (songStartingEvent.s === 0) {
+        this.actor.send({ type: 'COUNTDOWN_FINISHED' });
+      }
   }
 
   private songUploadedEventHandler: (roomEvent: RoomMessage) => void = (data) => {
     const songUploadedEvent = data as SongUploadedEvent;
-    this.transitionTo('Streaming', songUploadedEvent);
+    this.actor.send({ type: 'SONG_UPLOADED' },{ songUploadedEvent });
   }
   
 
   constructor(roomMessageListener: RoomMessageListener, restClient: RestClient, auxAudioPlayer: AuxAudioPlayer, analyser: AnalyserNode) {
+
     const optEl = document.getElementById("currently-playing");
     if (!optEl) throw new Error('No currently playing element found');
     this.el = optEl;
@@ -94,11 +263,6 @@ export class CurrentlyPlayingElement {
 
     this.auxAudioPlayer = auxAudioPlayer;
 
-    this.roomMessageListener.subscribe('SongUploadedEvent', (data) => {
-      const songUploadedEvent = data as SongUploadedEvent;
-      this.transitionTo('Streaming', songUploadedEvent);
-    });
-
     this.xState = 'Disconnected';
     this.transitionHistory = ['Disconnected'];
     this.audioVisualizer = new AudioVisualizer(analyser, audioCanvas);
@@ -107,6 +271,11 @@ export class CurrentlyPlayingElement {
     this.restClient = restClient;
     this.analyser = analyser;
     this.analyser.fftSize = 256;
+
+    this.actor.subscribe((state) => {
+      console.log(state);
+    });
+    this.actor.start();
 
   }
 
