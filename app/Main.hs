380f43b19d3672bd1fdd34ec8c84d4bded63ffe1
Fisnik <ukaj808@gmail.com>
2023-09-06 16:57:40 +0200

    fix allowing insecure; force TLS

--- app/Main.hs
+++ app/Main.hs
@@ -8,20 +8,27 @@ import CommandLine
 import System.IO.Temp (withTempDirectory)
 import Network.Wai.Handler.WarpTLS
 import Network.Wai.Middleware.ForceSSL
+import Network.Wai (Middleware)
+import Network.Wai.Request (appearsSecure)
 
+logSecure :: Middleware
+logSecure app req sendResponse =
+  app req $ \res -> do
+    putStrLn $ "Request Secure: " ++ show (appearsSecure req)
+    sendResponse res
 
 main :: IO ()
 main = do
   clArgs       <- getCLArgs
   withTempDirectory
-    "." 
+    "."
     "rooms"
     $ \roomsPath -> do
       roomRegistry <- newRegistry roomsPath
-      runTLS tlsOpts warOpts $ 
-        forceSSL             $ 
+      runTLS tlsOpts warOpts $
+        forceSSL             $
           server clArgs roomRegistry
   where
     port = 8443
-    tlsOpts = tlsSettings "tls/warp.crt" "tls/warp.key"
+    tlsOpts = (tlsSettings "tls/warp.crt" "tls/warp.key") { onInsecure = AllowInsecure }
     warOpts = setPort port defaultSettings
