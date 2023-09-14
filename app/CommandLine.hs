30ad32b4b5e9a762319aeb6a418ad7cdbb8a5297
fisnik <ukaj808@gmail.com>
2023-09-14 23:28:22 +0200

    debian

--- app/CommandLine.hs
+++ app/CommandLine.hs
@@ -11,12 +11,16 @@ newtype CLArgs =
   CLArgs
     { 
       staticAssetsPath      :: FilePath
+    , certificatePath       :: FilePath
+    , privateKeyPath        :: FilePath
     }
   deriving (Show)
 
 parseOptions :: Parser CLArgs
 parseOptions = CLArgs 
     <$> parseStaticAssetsPath 
+    <*> parseCertificatePath
+    <*> parsePrivateKeyPath
 
 parseStaticAssetsPath :: Parser FilePath
 parseStaticAssetsPath =
@@ -25,6 +29,24 @@ parseStaticAssetsPath =
       long "path-to-static-assets"
     , short 'p'
     , help "Directory of the static files"
+    ]
+
+parseCertificatePath :: Parser FilePath
+parseCertificatePath =
+  option str $ mconcat 
+    [
+      long "certificate-path"
+    , short 'c'
+    , help "Path to the certificate"
+    ]
+
+parsePrivateKeyPath :: Parser FilePath
+parsePrivateKeyPath =
+  option str $ mconcat 
+    [
+      long "private-key-path"
+    , short 'k'
+    , help "Path to the private key"
     ]
 
 getCLArgs :: IO CLArgs
