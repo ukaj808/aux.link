6a5635ba2f7c5d26b7aa40b046034a4a3eab6503
Fisnik <ukaj808@gmail.com>
2023-09-25 16:55:21 +0200

    testing view transitions api

--- html-demo/room.js
+++ html-demo/room.js
@@ -1,11 +1,10 @@
 const usersList = document.querySelector('.users-list');
-const offsetMultiplier = 100; //px
+const offsetMultiplier = 100; // px
 
 const refreshUserPosition = () => {
     const len = usersList.children.length;
     const usersListElements = Array.from(usersList.children);
     for (let i = 0, j = len - 1; i < len; i++,j--) {
-        console.log(usersListElements[i]);
         const offset = i * offsetMultiplier;
         const userEl = usersListElements[i];  
         userEl.style.left = offset;
@@ -17,6 +16,8 @@ const refreshUserPosition = () => {
 const updateView = (event) => {
     console.log('updating view...');
     const first = usersList.firstElementChild;
+    // This seems to be using a smooth-cross fade effect since were removing and adding the elements back to the dom?
+    // Maybe I can try just altering the styles properties instead...
     const sendFirstUserToBackOfList = () => {
         const user1 = usersList.firstElementChild;
         usersList.firstElementChild.remove();
