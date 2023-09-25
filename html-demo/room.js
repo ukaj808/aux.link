77c8f319fc19ad6df50f6ba7774f5fe8719e41b0
Fisnik <ukaj808@gmail.com>
2023-09-26 00:15:04 +0200

    testing out view transitions api with recursive approach that doesnt manipuate element order

--- html-demo/room.js
+++ html-demo/room.js
@@ -1,31 +1,37 @@
 const usersList = document.querySelector('.users-list');
-const offsetMultiplier = 100; // px
 
-const refreshUserPosition = () => {
-    const len = usersList.children.length;
-    const usersListElements = Array.from(usersList.children);
-    for (let i = 0, j = len - 1; i < len; i++,j--) {
-        const offset = i * offsetMultiplier;
-        const userEl = usersListElements[i];  
-        userEl.style.left = offset;
-        userEl.style.zIndex = j;
-    }
+const setInitialPositions = (node, offset, zIndex) => {
+    if (node === null) return;
+    node.style.left = offset;
+    node.style.zIndex = zIndex;
+    setInitialPositions(node.nextElementSibling, offset + 50, zIndex - 1);
 };
 
+const shiftUserElements = (head) => {
+    if (head === null) return;
+    const go = (node) => {
+        if (node === null) return;
+        if (node.nextElementSibling === null) {
+            const headPos = { left: head.style.left, zIndex: head.style.zIndex };
+            head.style.left = node.style.left;
+            head.style.zIndex = node.style.zIndex;
+            node.style.left = node.previousElementSibling.style.left;
+            node.style.zIndex = node.previousElementSibling.style.zIndex;
+            return headPos;
+        }
+       const headPos = go(node.nextElementSibling);
+       node.style.left = node.previousElementSibling.style.left;
+       node.style.zIndex = node.previousElementSibling.style.zIndex;
+       return headPos;
+    };
+    const headPos = go(head.nextElementSibling);
+    head.nextElementSibling.style.left = headPos.left;
+    head.nextElementSibling.style.zIndex = headPos.zIndex;
+}
 
 const updateView = (event) => {
-    console.log('updating view...');
-    const first = usersList.firstElementChild;
-    // This seems to be using a smooth-cross fade effect since were removing and adding the elements back to the dom?
-    // Maybe I can try just altering the styles properties instead...
-    const sendFirstUserToBackOfList = () => {
-        const user1 = usersList.firstElementChild;
-        usersList.firstElementChild.remove();
-        usersList.appendChild(user1);
-        refreshUserPosition();
-    }
-
-    const transition = document.startViewTransition(() => sendFirstUserToBackOfList());
+    const transition = document.startViewTransition(() => shiftUserElements(usersList.firstElementChild));
 }
-refreshUserPosition();
+
+setInitialPositions(usersList.firstElementChild, 0, usersList.children.length);
 usersList.addEventListener('click', updateView);
